3fb1a02f956bad7e35412aeb6e72b65ed184219b
Sushant <susaaant@gmail.com>
2026-08-01 22:28:20 +0545

    This is the code of ePay website.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,23 @@
 import { initializeApp, type FirebaseOptions } from 'firebase/app';
 import { getFirestore, type Firestore } from 'firebase/firestore';
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBqY7ldPjNAl8uVWkwqrtg-oyp8h-WC8Z4",
+//   authDomain: "sayyes-121.firebaseapp.com",
+//   projectId: "sayyes-121",
+//   storageBucket: "sayyes-121.firebasestorage.app",
+//   messagingSenderId: "743481099334",
+//   appId: "1:743481099334:web:84bd092dd62e7bb6db3647",
+//   measurementId: "G-4XG0WY2ERK"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBqY7ldPjNAl8uVWkwqrtg-oyp8h-WC8Z4",
-  authDomain: "sayyes-121.firebaseapp.com",
-  projectId: "sayyes-121",
-  storageBucket: "sayyes-121.firebasestorage.app",
-  messagingSenderId: "743481099334",
-  appId: "1:743481099334:web:84bd092dd62e7bb6db3647",
-  measurementId: "G-4XG0WY2ERK"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 export const isFirebaseConfigured = Boolean(firebaseConfig.apiKey);
